35d7248fafd8dcd909d9090b71424f28ea982b3e
Dang Hao <62649837+haotamht@users.noreply.github.com>
2023-07-12 04:11:05 +0700

    api change

--- src/Controllers/UserInfoController/index.js
+++ src/Controllers/UserInfoController/index.js
@@ -38,13 +38,46 @@ export const ordersInfo = async (req, res, next) => {
 export const addressInfo = async (req, res, next) => {
   try {
     const { _id } = req.dataUser;
-    const {} = req.body;
-    const resultFind = await User.findOne({ _id });
-    if (resultFind) {
-      const { phone, address } = resultFind;
-      return res.status(200).json({ status: 1, data: { phone, address } });
+    const { phone, address, name, tinh, huyen, xa } = req.body;
+    if (!address || !name || !tinh || !huyen || !xa) {
+      throw new Error("Thiếu thông tin");
+    }
+    const user = await User.findOne({ _id });
+
+    if (user) {
+      user.phone = phone;
+
+      user.address = {
+        address,
+        name,
+        tinh,
+        huyen,
+        xa,
+      };
+      await user.save();
+      return res
+        .status(200)
+        .json({ status: 1, data: { phone, address, name, tinh, huyen, xa } });
     } else {
-      return res.status(401).json({ status: 0, message: "No data find" });
+      throw new Error("No user found");
+    }
+  } catch (error) {
+    console.log(error.message);
+    next(error);
+  }
+};
+
+export const getAddressInfo = async (req, res, next) => {
+  try {
+    const { _id } = req.dataUser;
+    const user = await User.findOne({ _id });
+
+    if (user) {
+      return res
+        .status(200)
+        .json({ status: 1, data: { phone: user.phone, ...user.address } });
+    } else {
+      throw new Error("No user found");
     }
   } catch (error) {
     console.log(error.message);
--- src/Routes/UserInfoRoute/index.js
+++ src/Routes/UserInfoRoute/index.js
@@ -4,14 +4,23 @@ import {
   userInfo,
   ordersInfo,
   addressInfo,
+  getAddressInfo,
 } from "../../Controllers/UserInfoController/index.js";
 import { handleError } from "../../Controllers/ErrorController/index.js";
+import { ValidatePhone } from "../../Middleware/ValidatePhone/index.js";
 import { verifyAccessToken } from "../../Middleware/VeryfiToken/index.js";
 
 const userInfoRoute = express.Router();
 
 userInfoRoute.get("/", verifyAccessToken, userInfo, handleError);
 userInfoRoute.get("/orders", verifyAccessToken, ordersInfo, handleError);
-userInfoRoute.get("/address", verifyAccessToken, addressInfo, handleError);
+userInfoRoute.post(
+  "/address",
+  verifyAccessToken,
+  ValidatePhone,
+  addressInfo,
+  handleError
+);
+userInfoRoute.get("/address", verifyAccessToken, getAddressInfo, handleError);
 
 export default userInfoRoute;
